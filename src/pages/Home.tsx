ce7b51efa09a2341703e60f33aa3aca00a314020
julio <jbermudezfc90@gmail.com>
2026-07-24 12:30:09 -0400

    add file input

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Field, FieldDescription, FieldLabel } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
 import { CameraIcon, ImagesIcon } from 'lucide-react';
+import { useState } from 'react';
 
 export function Home() {
+    const [images, setImages] = useState<string[]>([]);
+
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const files = Array.from(event.target.files || []);
+        const fileUrls = files.map((file) => URL.createObjectURL(file));
+        setImages((prev) => [...prev, ...fileUrls]);
+    };
+
     return (
         <>
             <h3 className='text-2xl font-bold'>Detect your milo</h3>
@@ -11,6 +22,18 @@ export function Home() {
                     <CameraIcon className='size-6' />
                     Take photo
                 </Button>
+                <Field>
+                    <FieldLabel htmlFor='picture'>Picture</FieldLabel>
+                    <Input
+                        id='picture'
+                        type='file'
+                        accept='image/*'
+                        capture='environment'
+                        multiple
+                        onChange={handleImageChange}
+                    />
+                    <FieldDescription>Only JPG, PNG, GIF, and WebP files are supported.</FieldDescription>
+                </Field>
                 <Button
                     className='flex flex-1 flex-col px-6 py-12 border-2 border-primary text-primary'
                     variant='outline'
@@ -18,6 +41,19 @@ export function Home() {
                     <ImagesIcon className='size-6' />
                     Choose from gallery
                 </Button>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+                {images.map((image) => (
+                    <div className='relative' key={image}>
+                        <Button
+                            className='w-12 h-6 absolute inline-0 -right-3 -top-3 rounded-full text-center'
+                            variant='destructive'
+                        >
+                            x
+                        </Button>
+                        <img src={image} alt='Image' className='w-16 h-16 object-cover rounded-md' />
+                    </div>
+                ))}
             </div>
             <p className='text-sm text-muted-foreground'>Photograph through the microscope eyepiece.</p>
             <div className='flex justify-between'>

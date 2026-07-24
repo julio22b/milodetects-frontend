0929eb9851fe90326ee04d842a400a95fa1ec76f
julio <jbermudezfc90@gmail.com>
2026-07-24 12:36:40 -0400

    fix path

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import './index.css'
-import App from './App.tsx'
-import { Home } from './pages/Home.tsx'
-import { History } from './pages/History.tsx'
-import { ROUTES } from './lib/constants.ts'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import './index.css';
+import App from './App.tsx';
+import { Home } from './pages/Home.tsx';
+import { History } from './pages/History.tsx';
+import { ROUTES } from './lib/constants.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route index element={<Home />} />
-          <Route path={ROUTES.HISTORY} element={<History />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <StrictMode>
+        <BrowserRouter basename='/milodetects-frontend'>
+            <Routes>
+                <Route element={<App />}>
+                    <Route index element={<Home />} />
+                    <Route path={ROUTES.HISTORY} element={<History />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
+);
